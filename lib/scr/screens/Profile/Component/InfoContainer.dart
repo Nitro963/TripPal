5f97113c493fe98c73779ea35683db7fddf1512a
amgad123 <allnajjaramgad@gmail.com>
2021-04-18 05:39:51 +0300

    location

---
+++ lib/scr/screens/Profile/Component/InfoContainer.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:location/location.dart';
+import 'package:travel_app/scr/shared/Constants/constants.dart';
+import 'package:geocoder/geocoder.dart';
+import 'package:intl/intl.dart';
+
+class InfoContainer extends StatelessWidget {
+  final String leading;
+  final String trailling;
+  InfoContainer({this.leading, this.trailling});
+  @override
+  Widget build(BuildContext context) {
+    SizeConfig.init(context);
+    return GestureDetector(
+      child: Container(
+        // width: SizeConfig.blockSizeHorizontal*30,
+        padding: EdgeInsets.symmetric(horizontal: 12),
+        height: SizeConfig.blockSizeVertical * 6,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(50),
+            color: Colors.white,
+            boxShadow: [
+              BoxShadow(
+                  offset: Offset(1.8, 1.8),
+                  color: Theme.of(context).primaryColorLight)
+            ]),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Text(leading,
+                style: TextStyle(
+                  fontSize: 18,
+                    shadows: [
+                      Shadow(
+                          blurRadius: 2,
+                          offset: Offset(4, 4),
+                          color: Colors.grey.withOpacity(0.2))
+                    ],
+                    color: Colors.purpleAccent,
+                    fontStyle: FontStyle.italic,
+                    fontWeight: FontWeight.w700)),
+            SizedBox(width: SizeConfig.blockSizeHorizontal * 4),
+            Text(trailling,
+                style: TextStyle(
+                  fontStyle: FontStyle.italic,
+                  color: Colors.black54.withOpacity(0.6),
+                )),
+          ],
+        ),
+      ),
+      onTap: () async {
+       
+       
+      },
+    );
+  }
+}
---
+++ lib/scr/screens/Profile/Profile.dart
@@ -0,0 +1,187 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
+import 'package:travel_app/scr/screens/Profile/Component/InfoContainer.dart';
+import 'package:travel_app/scr/shared/Constants/constants.dart';
+import 'package:travel_app/scr/shared/Widgets/BottomNavigationBar.dart';
+import 'package:travel_app/scr/shared/services/location.dart';
+import 'package:image_picker/image_picker.dart';
+
+class ProfilePage extends StatefulWidget {
+  @override
+  _ProfilePageState createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  final _picker = ImagePicker();
+  File image;
+  @override
+  Widget build(BuildContext context) {
+    SizeConfig.init(context);
+   //  LocationCustom loc = Get.find<LocationCustom>();
+    List<RadioModel> sampleData = [
+      RadioModel(true, "Male"),
+      RadioModel(false, "female"),
+      RadioModel(false, "other")
+    ];
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      bottomNavigationBar: BottomNavBar(
+        currentIndex: 4,
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 12.0),
+          child: SingleChildScrollView(
+                      child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Align(
+                    alignment: Alignment.center,
+                    child: Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 12.0),
+                        child: Stack(
+                          children: [
+                            CircleAvatar(
+                              child: image == null
+                                  ? Icon(
+                                      Icons.person,
+                                      color: Colors.grey[500],
+                                      size: 60,
+                                    )
+                                  : null,
+                              radius: 80,
+                              foregroundImage:
+                                  image != null ? Image.file(image).image : null,
+                              backgroundColor: Colors.grey[100],
+                            ),
+                            Positioned(
+                                bottom: SizeConfig.blockSizeVertical * 3,
+                                right: SizeConfig.blockSizeHorizontal * .05,
+                                child: CircleAvatar(
+                                  child: GestureDetector(
+                                      onTap: () async {
+                                        final tmp = await _picker.getImage(
+                                            source: ImageSource.gallery);
+                                        print(tmp.path);
+                                        setState(() => image = File(tmp.path));
+                                      },
+                                      child: Icon(Icons.camera_alt,
+                                          color: Colors.white, size: 20)),
+                                  radius: 20,
+                                  backgroundColor: Theme.of(context)
+                                      .primaryColor
+                                      .withOpacity(0.7),
+                                )),
+                          ],
+                        ))),
+                Item(
+                  label: "First Name",
+                  value: "Amgad",
+                ),
+                Item(
+                  label: "Last Name",
+                  value: "All Najjar",
+                ),
+                SizedBox(height: SizeConfig.blockSizeVertical * 4),
+                Text("Gender",
+                    style: TextStyle(color: Colors.grey, fontSize: 19)),
+                Container(
+                  height: SizeConfig.blockSizeVertical * 10,
+                  child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: sampleData.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return InkWell(
+                            splashColor: Colors.blueAccent,
+                            onTap: () {
+                              setState(() {
+                                sampleData.forEach(
+                                    (element) => element.isSelected = false);
+                                sampleData[index].isSelected = true;
+                              });
+                            },
+                            child: RadioItem(sampleData[index]));
+                      }),
+                ),
+                Item(label:"Email Adress",value:"email@gmail.com"),
+                Item(label:"Phone",value:"0997493103"),
+                Item(label:"Location",value:"Damscus")
+
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Item extends StatelessWidget {
+  final String label;
+  final String value;
+  Item({this.label, this.value});
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 8.0),
+      child: TextFormField(
+        decoration: InputDecoration(
+          labelText: label,
+          labelStyle: TextStyle(color: Colors.grey, fontSize: 19),
+        ),
+        style: TextStyle(fontSize: 20, color: Colors.grey[700]),
+        initialValue: value,
+      ),
+    );
+  }
+}
+
+class RadioItem extends StatelessWidget {
+  final RadioModel _item;
+  RadioItem(this._item);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15.0),
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        children: <Widget>[
+          Container(
+              width: 18,
+              height: 18,
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: _item.isSelected ? Colors.transparent : Colors.black,
+                ),
+                borderRadius: BorderRadius.circular(4),
+                color:
+                    _item.isSelected ? Colors.blueAccent : Colors.transparent,
+              ),
+              child: Center(
+                  child: _item.isSelected
+                      ? Icon(
+                          Icons.check_sharp,
+                          color: Colors.white,
+                          size: 18,
+                        )
+                      : null)),
+          Container(
+            margin: EdgeInsets.only(left: 15.0),
+            child: Text(_item.text,
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600)),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class RadioModel {
+  bool isSelected;
+  final String text;
+
+  RadioModel(this.isSelected, this.text);
+}
