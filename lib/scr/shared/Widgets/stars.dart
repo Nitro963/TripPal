b038cd948e061f30e1eb77c446ec72cbda93ad97
amgad123 <allnajjaramgad@gmail.com>
2021-05-09 09:16:40 +0300

    Easy widget with animation

--- lib/scr/shared/Widgets/stars.dart
+++ lib/scr/shared/Widgets/stars.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:travel_app/scr/shared/Animation/Heart.dart';
+import 'package:travel_app/scr/shared/Animation/StarAnimation.dart';
 import 'package:travel_app/scr/shared/Constants/constants.dart';
-
+/*
 class StarsModel {
   final int starCount;
   final double starSize;
@@ -84,58 +86,95 @@ class _ClickableStarsState extends State<ClickableStars> {
       ],
     );
   }
-}
+}*/
 
 typedef void RatingChangeCallback(double rating);
 
-class StarRating extends StatelessWidget {
+class StarRating extends StatefulWidget {
   final int starCount;
   final double rating;
   final RatingChangeCallback onRatingChanged;
   final double size;
   final Color color;
   final bool isStatic;
+  final MainAxisAlignment mainAxisAlignment;
 
   StarRating(
       {this.starCount = 5,
+      this.mainAxisAlignment = MainAxisAlignment.start,
       this.isStatic = false,
       this.size = 15,
       this.rating = .0,
       this.onRatingChanged,
       this.color});
 
+  @override
+  _StarRatingState createState() => _StarRatingState();
+}
+
+class _StarRatingState extends State<StarRating> with TickerProviderStateMixin {
+  AnimationController rotationController;
+  @override
+  void initState() {
+    rotationController = AnimationController(
+        duration: const Duration(milliseconds: 5500), vsync: this);
+    super.initState();
+  }
+
   Widget buildStar(BuildContext context, int index) {
-    Icon icon;
-    if (index >= rating) {
+    Widget icon;
+    if (index >= widget.rating) {
       icon = new Icon(
         Icons.star_border,
-        size: size,
+        size: widget.size,
         color: Theme.of(context).buttonColor,
       );
-    } else if (index > rating - 1 && index < rating) {
-      icon = new Icon(
+    } else if (index > widget.rating - 1 && index < widget.rating) {
+     /* icon = new Icon(
         Icons.star_half,
-        size: size,
-        color: color ?? Theme.of(context).primaryColor,
+        size: widget.size,
+        color: widget.color ?? starsActivationColor,*/
+        icon = StarsAnimation(
+          startWidget: Icons.star_half,
+          endWidget: Icons.star_border,
+          beginColor: Colors.grey,
+          endColor: widget.color ?? starsActivationColor,
+          size: widget.size
+
       );
+    } else if (index == widget.rating - 1) {
+      icon = StarsAnimation(
+          startWidget: Icons.star,
+          endWidget: Icons.star_border,
+          beginColor: Colors.grey,
+          endColor: widget.color ?? starsActivationColor,
+          size: widget.size);
     } else {
       icon = new Icon(
         Icons.star,
-        size: size,
-        color: color ?? Theme.of(context).primaryColor,
+        size: widget.size,
+        color: widget.color ?? starsActivationColor,
       );
     }
     return new InkResponse(
-      onTap:
-          onRatingChanged == null ? null : () => onRatingChanged(index + 1.0),
-      child: icon,
-    );
+        onTap: widget.onRatingChanged == null
+            ? null
+            : () => widget.onRatingChanged(index + 1.0),
+        onDoubleTap: widget.onRatingChanged == null
+            ? null
+            : () => widget.onRatingChanged(index + .5),
+        child:
+            icon //SizedBox(height: widget.size,width:widget.size,child: icon),
+        );
   }
 
   @override
   Widget build(BuildContext context) {
     return new Row(
-        children: new List.generate(isStatic ? rating.ceil() : starCount,
+        mainAxisAlignment: widget.mainAxisAlignment,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: new List.generate(
+            widget.isStatic ? widget.rating.ceil() : widget.starCount,
             (index) => buildStar(context, index)));
   }
 }
