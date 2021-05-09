2ae76ba845cd1052fd6e70afbf371f55e092323c
amgad123 <allnajjaramgad@gmail.com>
2021-05-09 09:14:56 +0300

    Fixing Controlling Errors

--- lib/scr/screens/Hotels/Component/UpperNavBar.dart
+++ lib/scr/screens/Hotels/Component/UpperNavBar.dart
@@ -31,25 +31,30 @@ class UpperItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-        child: Container(
-            padding: EdgeInsets.symmetric(horizontal: 4, vertical: 2),
-            child: Row(
-              children: [
-                Icon(
-                  icon,
-                  color: Colors.blueAccent[700],
-                ),
-                SizedBox(width: SizeConfig.blockSizeHorizontal * 2),
-                Text(
-                  title,
-                  style: TextStyle(
-                      color: Colors.blueGrey,
-                      fontStyle: FontStyle.italic,
-                      fontSize: 15,
-                      fontWeight: FontWeight.w700),
-                )
-              ],
-            )),
+        child: Material(
+          elevation: 8,
+          borderRadius: BorderRadius.circular(20),
+          child: Container(
+              margin: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+              //decoration: BoxDecoration(color:Colors.red,borderRadius: BorderRadius.circular(20)),
+              child: Row(
+                children: [
+                  Icon(
+                    icon,
+                    color: Colors.blueAccent[700],
+                  ),
+                  SizedBox(width: SizeConfig.blockSizeHorizontal * 2),
+                  Text(
+                    title,
+                    style: TextStyle(
+                        color: Colors.blueGrey,
+                        fontStyle: FontStyle.italic,
+                        fontSize: 15,
+                        fontWeight: FontWeight.w700),
+                  )
+                ],
+              )),
+        ),
         onTap: onTapCallBack);
   }
 }
--- lib/scr/screens/Hotels/HotelSearchPage.dart
+++ lib/scr/screens/Hotels/HotelSearchPage.dart
@@ -2,17 +2,24 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:travel_app/scr/models/DemoData.dart';
 import 'package:travel_app/scr/screens/Hotels/HotelResults.dart';
+import 'package:travel_app/scr/screens/Hotels/HotelSearchPage.dart';
 import 'package:travel_app/scr/shared/Constants/constants.dart';
 import 'package:intl/intl.dart';
 import 'package:travel_app/scr/shared/Widgets/BottomNavigationBar.dart';
 import 'package:travel_app/scr/shared/services/validators.dart';
 
-class HotelSearchPage extends StatelessWidget {
-  String _city = '';
-  DateTimeRange _date;
-  int selectedIndex = 0;
+class HotelSearchController extends GetxController {
+  Rx<String> cityName = ''.obs;
+  Rx<String> date = ''.obs;
+  Rx<int> selectedIndex = 0.obs;
+}
+
+class HotelSearchPage extends GetView {
+  final HotelSearchController controller = HotelSearchController();
+
   @override
   Widget build(BuildContext context) {
+    SizeConfig.init();
     return Scaffold(
         resizeToAvoidBottomInset: false,
         bottomNavigationBar: BottomNavBar(
@@ -35,89 +42,111 @@ class HotelSearchPage extends StatelessWidget {
                     child: Text(
                         "TripPal will provide you what you really Need ",
                         style: TextStyle(
-                            fontSize: 30, color: Colors.lightBlue[700])),
+                            fontSize: 30,
+                            color: Colors.lightBlue[700],
+                            fontWeight: FontWeight.w500)),
                   ),
                   Padding(
                     padding: const EdgeInsets.symmetric(vertical: 15),
                     child: Text(
                       "Search for your perfect Hotel",
-                      style: TextStyle(fontSize: 15, color: Colors.blueGrey),
+                      style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w400,
+                          color: Colors.blueGrey),
                     ),
                   ),
                   Spacer(),
-                  // SizedBox(height: SizeConfig.blockSizeVertical * 5),
-
                   Expanded(
                     flex: 4,
                     child: Container(
                         padding: EdgeInsets.symmetric(horizontal: 20),
                         height: SizeConfig.blockSizeVertical * 40,
                         child: Column(children: [
-                          TextFormField(
-                            decoration: InputDecoration(
-                                border: OutlineInputBorder(),
-                                enabledBorder: OutlineInputBorder(
-                                    borderSide: BorderSide(
-                                        color: _city != ''
-                                            ? validateColor
-                                            : deactivatedColor)),
-                                hintText: "City",
-                                prefixIcon: Icon(Icons.search),
-                                hintStyle: TextStyle(color: Colors.grey[300])),
-                            validator: validateCity,
-                            onSaved: (val) => _city = val,
+                          Obx(
+                            () => TextField(
+                                decoration: InputDecoration(
+                                    border: OutlineInputBorder(),
+                                    enabledBorder: OutlineInputBorder(
+                                        borderSide: BorderSide(
+                                            color:
+                                                controller.cityName.value != ""
+                                                    ? activationColor
+                                                    : deactivatedColor)),
+                                    hintText: "City",
+                                    prefixIcon: Icon(Icons.search),
+                                    hintStyle: TextStyle(
+                                        fontSize: 17,
+                                        color: Colors.grey,
+                                        fontWeight: FontWeight.w500)),
+                                // validator: validateCity,
+                                onChanged: (val) =>
+                                    controller.cityName.value = val),
                           ),
                           SizedBox(height: SizeConfig.blockSizeVertical * 5),
-                          GestureDetector(
-                              child: Container(
-                                decoration: BoxDecoration(
-                                    border: Border(
-                                        left: BorderSide(
-                                  color: _date != null
-                                      ? validateColor
-                                      : deactivatedColor,
-                                  width: 3,
-                                ))),
-                                child: Row(
-                                  children: [
-                                    SizedBox(width: 8),
-                                    Icon(
-                                      Icons.calendar_today_outlined,
-                                      color: deactivatedColor,
-                                    ),
-                                    SizedBox(width: 10),
-                                    Text(_date == null
-                                            ? "From - To"
-                                            : "${DateFormat(DateFormat.MONTH_DAY).format(_date.start)} - ${DateFormat(DateFormat.MONTH_DAY).format(_date.end)}"
-                                        //style:TextStyle()
-                                        )
-                                  ],
+                          Obx(
+                            () => GestureDetector(
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                      border: Border(
+                                          left: BorderSide(
+                                    color: controller.date.value != ""
+                                        ? activationColor
+                                        : deactivatedColor,
+                                    width: 3,
+                                  ))),
+                                  child: Row(
+                                    children: [
+                                      SizedBox(width: 8),
+                                      Icon(
+                                        Icons.calendar_today_outlined,
+                                        color: deactivatedColor,
+                                      ),
+                                      SizedBox(width: 10),
+                                      Text(controller.date.value == ""
+                                              ? "From - To"
+                                              : controller.date.value
+                                          //style:TextStyle()
+                                          )
+                                    ],
+                                  ),
                                 ),
-                              ),
-                              onTap: () async {
-                                _date = await showDateRangePicker(
-                                    context: context,
-                                    firstDate: DateTime.now(),
-                                    lastDate: DateTime.now()
-                                        .add(Duration(days: 365)));
-                              }),
+                                onTap: () async {
+                                  DateTimeRange _tmp =
+                                      await showDateRangePicker(
+                                          context: context,
+                                          firstDate: DateTime.now(),
+                                          lastDate: DateTime.now()
+                                              .add(Duration(days: 365)));
+                                  controller.date.value =
+                                      DateFormat(DateFormat.MONTH_DAY)
+                                              .format(_tmp.start) +
+                                          " - " +
+                                          DateFormat(DateFormat.MONTH_DAY)
+                                              .format(_tmp.end);
+                                }),
+                          ),
                           SizedBox(height: SizeConfig.blockSizeVertical * 5),
                           DropdownButtonFormField<String>(
                             decoration: InputDecoration(
                                 border: OutlineInputBorder(),
                                 enabledBorder: OutlineInputBorder(
                                     borderSide: BorderSide(
-                                        color: _city != ''
-                                            ? validateColor
-                                            : deactivatedColor)),
+                                        color:
+                                            controller.selectedIndex.value != -1
+                                                ? activationColor
+                                                : deactivatedColor)),
                                 contentPadding: EdgeInsets.all(12)),
                             icon: const Icon(Icons.arrow_downward),
                             iconSize: 24,
                             elevation: 16,
                             style: const TextStyle(color: Colors.deepPurple),
-                            value: rooms[selectedIndex],
+                            value: controller.selectedIndex.value != -1
+                                ? rooms[controller.selectedIndex.value]
+                                : "please Select",
                             onChanged: (val) {
-                              selectedIndex = rooms.indexOf(val);
+                              controller.selectedIndex.value =
+                                  rooms.indexOf(val);
                             },
                             items: rooms.map((String item) {
                               return DropdownMenuItem<String>(
@@ -130,17 +159,26 @@ class HotelSearchPage extends StatelessWidget {
                           Spacer(
                             flex: 2,
                           ),
-                          FlatButton(
-                            minWidth: SizeConfig.blockSizeHorizontal * 90,
+                          ElevatedButton(
+                            style: ButtonStyle(
+                                minimumSize: MaterialStateProperty.all<Size>(
+                                    Size(SizeConfig.blockSizeHorizontal * 70,
+                                        SizeConfig.blockSizeVertical * 6))),
                             child: Text(
                               "Search",
-                              style: TextStyle(color: Colors.white),
+                              style: TextStyle(
+                                  fontSize: 18,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w500,
+                                  fontStyle: FontStyle.italic),
                             ),
-                            color: Colors.blue[700],
                             onPressed: () {
+                              print(controller.cityName.value);
+                              print(controller.date.value);
+                              print(controller.selectedIndex.value);
                               Get.to(HotelResult());
                             },
-                          ),
+                          )
                         ])),
                   ),
                 ],
